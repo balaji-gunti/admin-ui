b09aabd0e745e76a6d0bb9367d77e168e6cd0e60
Balaji <balajigunti178@gmail.com>
2023-04-21 16:01:31 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import LandingPage from "./components/LandingPage";
+import config from "./conf/config.js";
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    getUsersData();
+  }, []);
+
+  const getUsersData = async () => {
+    try {
+      const response = await axios.get(config.endpoint);
+      console.log(response.data);
+      setUsers(response.data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  return <LandingPage users={users} setUsers={setUsers} />;
 }
 
 export default App;
